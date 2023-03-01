ef8ca67b99b03ab392b7736f00a332005add41cd
jjugwen <yjgwen@gmail.com>
2023-03-01 23:03:27 +0900

    Update server code
    라이브러리 설치
    1. xhr2 :  XMLHttpRequest 사용 위함.
    2. cors: cors 충돌 해결 위함.
    
    클라이언트-서버 데이터
    get : date, currencylist, nationlist 보내기
    post: 두 국가의 통화코드값 받으면, rate 값 보내기

--- app.js
+++ app.js
@@ -1,12 +1,78 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8080;
+const XMLHttpRequest = require("xhr2");
 
-app.get('/', (req, res) => {
-  res.send("Hello World~")
-})
+//cors 추가하면 ajax 잘됨 npm install cors
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("HOME");
+});
+
+//국가 통화코드 currency1, currency2 받으면, exchangeRate값 보내기
+app.post("/api/:currency1/:currency2", (req, res) => {
+  // app.post("/api/:currency", (req, res) => {
+  // const currency1 = "USD";
+  // const currency2 = "KRW";
+  // console.log(req.params);
+  const { currency1, currency2 } = req.params;
+  const requestURL = `https://api.exchangerate.host/convert?from=${currency1}&to=${currency2}`;
+  const request = new XMLHttpRequest();
+  request.open("GET", requestURL);
+  request.responseType = "json";
+  request.send();
+
+  request.onload = function () {
+    const response = request.response;
+    const exchangeRate = response.info.rate;
+    res.send(`${exchangeRate}`);
+    // console.log(exchangeRate)
+  };
+});
+
+//date, currencylist 보내기
+app.get("/currencylist", (req, res) => {
+  const currency = "USD";
+  const requestURL = `https://api.exchangerate.host/latest?base=${currency}`;
+  const request = new XMLHttpRequest();
+  request.open("GET", requestURL);
+  request.responseType = "json";
+  request.send();
+
+  request.onload = function () {
+    const response = request.response;
+    // console.log(response);
+    const date = response.date;
+    const currencyList = Object.keys(response.rates);
+    // res.send(date);
+    // res.send(currencyList);
+    res.send({ date, currencyList });
+  };
+});
+
+//국가, 통화단위(symbols) 보내기
+app.get("/nationlist", (req, res) => {
+  const requestURL = "https://api.exchangerate.host/symbols";
+  const request = new XMLHttpRequest();
+  request.open("GET", requestURL);
+  request.responseType = "json";
+  request.send();
+
+  request.onload = function () {
+    const response = request.response;
+    // console.log(response.symbols)
+    const data = Object.values(response.symbols);
+    // console.log(data);
+    res.send(data);
+  };
+});
 
 //서버 실행
 app.listen(port, () => {
-  console.log("서버 실행!!!")
-})
+  console.log("서버 실행!!!");
+});
