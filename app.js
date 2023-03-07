1ed9a3c5d87b2b162640c5d0714fe7b19e1887e1
jjugwen <yjgwen@gmail.com>
2023-03-07 18:14:31 +0900

    Update app.js
    불필요한 코드 삭제

--- app.js
+++ app.js
@@ -4,19 +4,12 @@ const port = 8080;
 const XMLHttpRequest = require("xhr2");
 
 //cors 추가하면 ajax 잘됨 npm install cors
-app.use(express.json());
 const cors = require("cors");
 const corsOptions = {
   origin: 'http://localhost:3000',
   credential: true
 }
 app.use(cors(corsOptions));
-
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.send("HOME");
-});
 
 //국가 통화코드 from(currencyCodeFrom), to(currencyCodeTo) 받으면, exchangeRate, date 보내기
 app.post("/api", (req, res) => {
@@ -27,21 +20,24 @@ app.post("/api", (req, res) => {
   request.responseType = "json";
   request.send();
 
-  request.onload = function () {
-    const response = request.response;
-    const date = response.date;
-    const exchangeRate = response.info.rate;
-    res.send({exchangeRate, date});
+  request.onload = function () { 
+    if(request.status === 200 || request.status === 201){
+      const response = request.response;
+      const date = response.date;
+      const exchangeRate = response.info.rate;
+      res.send({exchangeRate, date});
+    }else {
+      console.error(request.responseText);
+    }
   };
 });
 
 //국가, 통화단위 보내기
 //select ver.2 - currencyNationList.json
 //http://shancarter.github.io/mr-data-converter/ 에서 표를 JSON-Dictionary 로 변환
-const listData = require('./currencyNationList.json');
 app.get("/currencynationlist", (req, res) => {
-  const data = listData
-  res.send({...data})
+  const listData = require('./currencyNationList.json');
+  res.send({...listData})
 })
 
 //서버 실행
